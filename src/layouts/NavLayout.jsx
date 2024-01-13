d99c59a45e53a1168ea1363b22c14dcee3303fa7
Evalyn Berleant <eberleant@gmail.com>
2024-01-12 19:23:46 -0500

    WIP - Navigation Layout styling

--- src/layouts/NavLayout.jsx
+++ src/layouts/NavLayout.jsx
@@ -4,30 +4,33 @@ import { Outlet, Link } from 'react-router-dom';
 export default function NavLayout() {
 
   return (
-    <div className="d-flex flex-row nav-layout page-container">
-      <div className="left-menu">
-        <div className="menu-group">
-          <span className="menu-group-header">MISSIONS</span>
-          <Link to="/" className="menu-group-item">Mission I</Link>
+    <div className="nav-layout primary page-container">
+      <h1>CTRL+ZZZ HQ</h1>
+      <div className="d-flex flex-row w-100 secondary">
+        <div className="left-menu">
+          <div className="menu-group">
+            <span className="menu-group-header">MISSIONS</span>
+            <Link to="/" className="menu-group-item">Mission I</Link>
+          </div>
+          <div className="menu-group">
+            <span className="menu-group-header">AGENTS</span>
+            <Link to="/" className="menu-group-item">I [xxx]</Link>
+            <Link to="/" className="menu-group-item">II [xxx]</Link>
+            <Link to="/" className="menu-group-item">III [xxx]</Link>
+          </div>
+          <div className="menu-group">
+            <span className="menu-group-header">TARGETS</span>
+            <Link to="/" className="menu-group-item">I [xxx]</Link>
+            <Link to="/" className="menu-group-item">II [xxx]</Link>
+            <Link to="/" className="menu-group-item">III [xxx]</Link>
+          </div>
+          <div className="menu-group">
+            <Link to="/" className="menu-group-header">DREAM LOG</Link>
+          </div>
         </div>
-        <div className="menu-group">
-          <span className="menu-group-header">AGENTS</span>
-          <Link to="/" className="menu-group-item">I (codename: xxx)</Link>
-          <Link to="/" className="menu-group-item">II (codename: xxx)</Link>
-          <Link to="/" className="menu-group-item">III (codename: xxx)</Link>
+        <div className="page-content">
+          <Outlet />
         </div>
-        <div className="menu-group">
-          <span className="menu-group-header">TARGETS</span>
-          <Link to="/" className="menu-group-item">I (codename: xxx)</Link>
-          <Link to="/" className="menu-group-item">II (codename: xxx)</Link>
-          <Link to="/" className="menu-group-item">III (codename: xxx)</Link>
-        </div>
-        <div className="menu-group">
-          <Link to="/" className="menu-group-header">DREAM LOG</Link>
-        </div>
-      </div>
-      <div className="page-content">
-        <Outlet />
       </div>
     </div>
   );
