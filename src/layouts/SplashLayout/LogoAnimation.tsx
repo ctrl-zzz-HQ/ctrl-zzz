671cab63f9c7dbf225723731e57179703e6d3e33
Evalyn Berleant <eberleant@gmail.com>
2024-01-21 16:36:46 -0500

    Fix logo animation playback on Firefox and Safari

--- src/layouts/SplashLayout/LogoAnimation.tsx
+++ src/layouts/SplashLayout/LogoAnimation.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
 import logoAnimationWebm from '../../assets/Logo 500x500.webm';
+import logoAnimationMov from '../../assets/Logo 500x500.mov';
 import logoAnimationMp4 from '../../assets/Logo 500x500.mp4';
 
 export default function LogoAnimation({ play, onEnded }: LogoAnimationProps) {
@@ -10,6 +11,10 @@ export default function LogoAnimation({ play, onEnded }: LogoAnimationProps) {
   useEffect(() => {
     if (play) {
       if (logoAnimationRef.current && playLogoAnimation) {
+        if (window.navigator.userAgent.includes('Firefox')) {
+          // Required to consistently start from beginning on Firefox
+          logoAnimationRef.current.load();
+        }
         logoAnimationRef.current.play();
       } else {
         onEnded(); // signal to end immediately
@@ -19,9 +24,10 @@ export default function LogoAnimation({ play, onEnded }: LogoAnimationProps) {
 
   return (
     <div className={`logo-wrapper ${play ? '' : 'd-none'}`}>
-      <video playsInline muted className="logo-animation" ref={logoAnimationRef} onEnded={onEnded}>
-        <source src={logoAnimationWebm} type="video/webm"/>
-        <source src={logoAnimationMp4} type="video/mp4" onError={() => setPlayLogoAnimation(false)}/>
+      <video playsInline muted className="logo-animation" ref={logoAnimationRef} onStalled={onEnded} onEnded={onEnded}>
+        <source src={logoAnimationMov} type="video/quicktime"/> {/* Safari - must be first to avoid playing WEBM without transparency */}
+        <source src={logoAnimationWebm} type="video/webm"/> {/* Chrome/Firefox */}
+        <source src={logoAnimationMp4} type="video/mp4" onError={() => setPlayLogoAnimation(false)}/> {/* Backup - no transparency */}
       </video>
     </div>
   );
