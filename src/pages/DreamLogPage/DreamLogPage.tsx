275c0c82b35cccb3d25d8d8ee2ec0cc8a6d1e4d5
Evalyn Berleant <eberleant@gmail.com>
2024-01-25 16:37:40 -0500

    Temporarily remove scrolling functionality - may add back when it is ready

--- src/pages/DreamLogPage/DreamLogPage.tsx
+++ src/pages/DreamLogPage/DreamLogPage.tsx
@@ -1,23 +1,12 @@
 import styles from './DreamLogPage.module.css';
 import dreamLogs from '@data/dream-logs';
-import { useMemo, useEffect, useCallback, useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import TypingAnimation from '@components/TypingAnimation';
 
 export default function DreamLog({ index }: Props) {
 
   const dreamLog = useMemo(() => dreamLogs[index], [index]);
   const bodyRef = useRef(null);
-
-  const scrollToNextPage = useCallback(() => {
-    if (bodyRef.current) {
-      bodyRef.current.scrollTop = bodyRef.current.scrollTop + bodyRef.current.clientHeight;
-    }
-  }, []);
-
-  useEffect(() => {
-    document.addEventListener('keydown', scrollToNextPage);
-    return () => document.removeEventListener('keydown', scrollToNextPage);
-  }, [scrollToNextPage]);
 
   return (
     <>
@@ -30,9 +19,6 @@ export default function DreamLog({ index }: Props) {
       <div className={styles.scrollBody} ref={bodyRef}>
         <TypingAnimation text={dreamLog.text} playTrigger={1} />
       </div>
-      <p className={`${styles.continueText} secondary-text desktop`}>
-        [press any key to continue]
-      </p>
     </>
   );
 }
