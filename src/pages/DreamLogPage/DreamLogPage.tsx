c44365e5c291308107d11751429a561b18a30b2e
Evalyn Berleant <eberleant@gmail.com>
2024-01-25 12:37:17 -0500

    wip - Scroll down when key is pressed

--- src/pages/DreamLogPage/DreamLogPage.tsx
+++ src/pages/DreamLogPage/DreamLogPage.tsx
@@ -1,11 +1,23 @@
 import styles from './DreamLogPage.module.css';
 import dreamLogs from '@data/dream-logs';
-import { useMemo } from 'react';
+import { useMemo, useEffect, useCallback, useRef } from 'react';
 import TypingAnimation from '@components/TypingAnimation';
 
 export default function DreamLog({ index }: Props) {
 
   const dreamLog = useMemo(() => dreamLogs[index], [index]);
+  const bodyRef = useRef(null);
+
+  const scrollToNextPage = useCallback(() => {
+    if (bodyRef.current) {
+      bodyRef.current.scrollTop = bodyRef.current.scrollTop + bodyRef.current.clientHeight;
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('keydown', scrollToNextPage);
+    return () => document.removeEventListener('keydown', scrollToNextPage);
+  }, [scrollToNextPage]);
 
   return (
     <>
@@ -15,12 +27,12 @@ export default function DreamLog({ index }: Props) {
         <span className="desktop">timestamp: </span>
         {dreamLog.timestamp}]
       </h2>
-      <div className="scrollable">
+      <div className={styles.scrollBody} ref={bodyRef}>
         <TypingAnimation text={dreamLog.text} playTrigger={1} />
       </div>
-      <button className={`${styles.continueButton} secondary-text`}>
-        [click here <span className="desktop">or press any key</span> to continue]
-      </button>
+      <p className={`${styles.continueText} secondary-text desktop`}>
+        [press any key to continue]
+      </p>
     </>
   );
 }
