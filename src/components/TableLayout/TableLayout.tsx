2e758a524457a20ab17c37876a4f47f4e2bc05b9
Evalyn Berleant <eberleant@gmail.com>
2024-01-26 23:22:36 -0500

    Refactor swipe logic into custom hook

--- src/components/TableLayout/TableLayout.tsx
+++ src/components/TableLayout/TableLayout.tsx
@@ -1,47 +1,25 @@
 import styles from './TableLayout.module.css';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { useState, useCallback, TouchEvent } from 'react';
-import { usePathIndex } from '@/hooks';
+import { useCallback } from 'react';
+import { usePathIndex, useSwipe } from '@/hooks';
 import { DesktopLinks, MobileLinks } from '@components/PageLinks';
 import Footer from '@components/Footer';
-
-// the required distance between touchStart and touchEnd to be detected as a swipe
-const minSwipeDistance = 50
 
 export default function TableLayout({ data }: Props) {
 
   const navigate = useNavigate();
   const currIndex = usePathIndex();
-
-  // Swipe logic adapted from: https://stackoverflow.com/a/70612770
-  const [touchStart, setTouchStart] = useState<number|null>(null)
-  const [touchEnd, setTouchEnd] = useState<number|null>(null)
-
-  const onTouchStart = useCallback((e: TouchEvent) => {
-    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
-    setTouchStart(e.targetTouches[0].clientX)
-  }, [])
-
-  const onTouchMove = useCallback((e: TouchEvent) => setTouchEnd(e.targetTouches[0].clientX), [])
-
-  const onTouchEnd = useCallback(() => {
+  const swipeHandlers = useSwipe(useCallback(direction => {
     if (currIndex === undefined) return;
-    if (!touchStart || !touchEnd) return;
-
-    const distance = touchStart - touchEnd
-    const isLeftSwipe = distance > minSwipeDistance
-    const isRightSwipe = distance < -minSwipeDistance
-    if (!isLeftSwipe && !isRightSwipe) return;
-
-    if (isLeftSwipe) {
+    if (direction == 'left') {
       if (currIndex < data.length - 1) navigate(`${currIndex + 1}`);
-    } else if (isRightSwipe) {
+    } else if (direction == 'right') {
       if (currIndex > 0) navigate(`${currIndex - 1}`);
     }
-  }, [touchStart, touchEnd, currIndex])
+  }, [data, currIndex, navigate]));
 
   return (
-    <div className={styles.tableContainer} onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
+    <div className={styles.tableContainer} {...swipeHandlers}>
       <div className={styles.table}>
         <div className={`${styles.row} ${styles.fillHeight}`}>
           <div className={`${styles.cell} ${styles.fitWidth} desktop`}>
--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,5 @@
 import { useLocation } from 'react-router-dom';
-import { useMemo } from 'react';
+import { useMemo, useState, useCallback, TouchEvent } from 'react';
 
 export const usePathIndex = function(): number|undefined {
   const { pathname } = useLocation();
@@ -13,3 +13,40 @@ export const usePathIndex = function(): number|undefined {
   }, [pathname]);
   return currIndex;
 }
+
+// Swipe logic adapted from: https://stackoverflow.com/a/70612770
+export const useSwipe = function(
+  callback: (swipeDir: direction) => void,
+  options?: { swipeDistance?: number }) {
+
+  let swipeDistance = options?.swipeDistance || 50;
+
+  const [touchStart, setTouchStart] = useState<number|null>(null)
+  const [touchEnd, setTouchEnd] = useState<number|null>(null)
+
+  const onTouchStart = useCallback((e: TouchEvent) => {
+    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX)
+  }, [])
+
+  const onTouchMove = useCallback((e: TouchEvent) => setTouchEnd(e.targetTouches[0].clientX), [])
+
+  const onTouchEnd = useCallback(() => {
+    if (!touchStart || !touchEnd) return;
+
+    const distance = touchStart - touchEnd
+    const isLeftSwipe = distance > swipeDistance
+    const isRightSwipe = distance < -swipeDistance
+
+    let swipeDir: direction|undefined;
+
+    if (isLeftSwipe) swipeDir = 'left';
+    if (isRightSwipe) swipeDir = 'right';
+
+    if (swipeDir) callback(swipeDir);
+  }, [touchStart, touchEnd]);
+
+  return { onTouchStart, onTouchMove, onTouchEnd };
+}
+
+type direction = 'left' | 'right' | 'up' | 'down';
